a3e80ebc4c8bcc3302dec24f22073feca56f3be7
Duy DANG <duy.dang@wanadoo.fr>
2016-08-19 16:54:29 +0000

    Afficher horaire en format date et link_to sur le titre dans index

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,15 @@
 class Event < ApplicationRecord
 
+    # Desciption avec le N premieres lettres seulement du champs
     def description_courte
-      return self.description[0..20]+" ..."
+      if self.description
+          return self.description[1..10]
+      end
+    end
+
+    # Date sous forme jour mois année
+    def horaire_affiche
+      text_horaire = self.horaire.to_s
+      text_aff = text_horaire[8..9] +"-"+text_horaire[5..6]+"-"+text_horaire[0..3]
     end
 end
