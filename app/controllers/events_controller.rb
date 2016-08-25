c2507dba8145e588a0846913d2b0857d5590e832
Duy DANG <duy.dang@wanadoo.fr>
2016-08-25 23:21:58 +0000

    fonction tri par profil

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,11 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    if params[:user]
+        @events = Event.where(user: params[:user])
+    else
+       @events = Event.all
+    end
   end
 
   # GET /events/1
