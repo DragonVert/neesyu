3a8be36bb86c677dd363de9a49fb356e30d32d70
Duy DANG <duy.dang@wanadoo.fr>
2016-08-26 17:30:09 +0000

    ajouter icons font awasome

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :profil
   has_many :events
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable :registerable,
-  devise :database_authenticatable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,:registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
