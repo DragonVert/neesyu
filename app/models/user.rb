2c38400834708d3989072fca04cb5de071f4248b
Duy DANG <duy.dang@wanadoo.fr>
2016-08-26 02:01:09 +0000

    empecher l'enregistrement

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :profil
   has_many :events
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable and :omniauthable :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
