b12ca964731fb1e610ae1ac2ea8ab9add07bfe25
Duy DANG <duy.dang@wanadoo.fr>
2016-08-27 13:39:13 +0000

    controller pages search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,37 @@
 class PagesController < ApplicationController
   def home
   end
+  def search
+        if params[:search]
+            @ligne = params[:search]
+
+            @events = search_string(@ligne)
+
+            @events.sort_by! { |e| e.debut }
+
+            if @events.count > 1
+                render "events/index"
+            else
+                flash[:notice] = "Aucun résultat à votre recherche"
+                redirect_to :back
+            end
+        end
+  end
+
+  def search_string(mot)
+
+        # Liste des evenements
+        @events = Event.all
+
+        # recherche par nom
+        @events_nom = @events.select{ |e| e.user.profil.nom.include?(mot)}
+
+        # recherche par prenom
+        @events_prenom = @events.select{ |e| e.user.profil.prenom.include?(mot)}
+
+        # recherche par titre
+        @events_titre = @events.select{ |e| e.titre.include?(mot)}
+
+        @events = @events_titre + @events_nom + @events_prenom
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   root to: 'pages#home'
+
+  # resultats de recherche
+  get '/pages/search', to: 'pages#search', as: 'search'
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
